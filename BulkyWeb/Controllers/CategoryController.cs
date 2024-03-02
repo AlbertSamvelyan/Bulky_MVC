4fc1832b061897f0babf0081b220bde09a8a6968
AlbertSamvelyan <albertsamvelyan@gmail.com>
2024-03-03 01:12:48 +0400

    Add functionality to edit existing categories.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -28,13 +28,37 @@ namespace BulkyWeb.Controllers
             if (obj.Name == obj.DisplayOrder.ToString()) {
                 ModelState.AddModelError("name", "The DisplayOrder can not exactly match the Name.");
             }
-            if (ModelState.IsValid)
+			if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
 				return RedirectToAction("Index", "Category");
 			}
             return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0) {
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+			//Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null) {
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
 		}
 	}
 }
